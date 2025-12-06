9c735a8bdd88ff8d27e47fd3a5128ec6ecf42bc6
Samia <samiamougna@gmail.com>
2025-12-06 13:13:32 +0100

    structure done

---
+++ models/schema.js
@@ -0,0 +1,15 @@
+//TEMPLATE
+
+// const mongoose = require('mongoose');
+// const placeSchema = mongoose.Schema({
+
+// nickname: String, 
+// name: String,
+// longitude: Number,
+// latitude: Number,
+
+// });
+
+
+// const Place = mongoose.model('places', placeSchema);
+// module.exports = Place;
